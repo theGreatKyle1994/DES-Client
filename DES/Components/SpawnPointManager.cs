c5fb172f917b735719fe39e68caf0e3ce82ff836
kylerm <kylemorey1994@gmail.com>
2026-02-14 16:36:19 -0500

    fix screen placement of markers

--- DES/Components/SpawnPointManager.cs
+++ DES/Components/SpawnPointManager.cs
@@ -27,11 +27,10 @@ public class SpawnPointManager : MonoBehaviour
 		if (CameraClass.Instance.SSAA.isActiveAndEnabled)
 		{
 			_screenScale = (float)CameraClass.Instance.SSAA.GetOutputWidth() /
-			               CameraClass.Instance.SSAA.GetOutputHeight();
+			               CameraClass.Instance.SSAA.GetInputWidth();
 		}
 		_gameSpawnPoints = FindObjectsOfType<SpawnPointMarker>().ToList();
 		SortSpawnsByZones();
-		CreateSpawnMarkers();
 	}
 	
 	private void OnGUI()
@@ -47,23 +46,23 @@ public class SpawnPointManager : MonoBehaviour
 		{
 			foreach (var spawnInstance in _spawnZoneGroups[zoneGroup])
 			{
-				// Limit distance of markers
-				var camPos = _cam.transform.position;
-				var dist = Mathf.RoundToInt((spawnInstance.Position - camPos).magnitude);
-				if (spawnInstance.GUIText.text.Length <= 0 || !(dist < 300f)) continue;
-				
 				// Ignore behind camera
-				var screenPos = _cam.WorldToScreenPoint(spawnInstance.Position + (Vector3.up * 1.2f));
+				var screenPos = _cam.WorldToScreenPoint(spawnInstance.Marker.transform.position + (Vector3.up * 1.5f));
 				if (screenPos.z <= 0) continue;
 				
+				// Limit distance of markers
+				var camPos = _cam.transform.position;
+				var dist = Mathf.RoundToInt((spawnInstance.Marker.transform.position - camPos).magnitude);
+				if (spawnInstance.GUIText.text.Length <= 0 || !(dist < 300f)) continue;
+				
 				// Set gui box size / postion
-				var guiSize = _guiStyle!.CalcSize(spawnInstance.GUIText);
-				spawnInstance.Display.x = (screenPos.x * _screenScale);
+				var guiSize = _guiStyle.CalcSize(spawnInstance.GUIText);
+				spawnInstance.Display.x = (screenPos.x * _screenScale) - (guiSize.x / 2);
 				spawnInstance.Display.y = Screen.height - ((screenPos.y * _screenScale) + guiSize.y);
 				spawnInstance.Display.size = guiSize;
 				
 				// Use distance fade out
-				var alpha = ((dist - 150f) / (0f - 150f)) * 1f;
+				var alpha = (dist - 150f) / (0f - 150f) * 1f;
 				alpha = alpha switch { > 1f => 1f, < 0.01f => 0f, _ => alpha };
 				GUI.contentColor = new Color(1, 1, 1, alpha);
 				GUI.backgroundColor = new Color(0, 0, 0, alpha);
@@ -84,48 +83,44 @@ public class SpawnPointManager : MonoBehaviour
 		{
 			if (spawn.SpawnPoint.BotZoneName != null)
 			{
-				if (_spawnZoneGroups.ContainsKey(spawn.SpawnPoint.BotZoneName)) continue;
-				_spawnZoneGroups.Add(spawn.SpawnPoint.BotZoneName, []);
-				_zoneNames.Add(spawn.SpawnPoint.BotZoneName);
+				if (!_spawnZoneGroups.ContainsKey(spawn.SpawnPoint.BotZoneName))
+				{
+					_spawnZoneGroups.Add(spawn.SpawnPoint.BotZoneName, []);
+					_zoneNames.Add(spawn.SpawnPoint.BotZoneName);
+				}
 			} else if (!_spawnZoneGroups.ContainsKey("ungrouped")) { _spawnZoneGroups.Add("ungrouped", []); }
+			CreateSpawnMarker(spawn);
 		}
 		Log($"Found {_zoneNames.Count} total zones.");
 	}
 	
-	private void CreateSpawnMarkers()
+	private void CreateSpawnMarker(SpawnPointMarker spawn)
 	{
-		foreach (var spawn in _gameSpawnPoints)
+		// Create marker text
+		_sb.Clear();
+		_sb.AppendFormat($"Position | ");
+		_sb.AppendFormat($"( X:{spawn.Position.x:F2} Y:{spawn.Position.y:F2} Z:{spawn.Position.z:F2} )");
+		
+		// _sb.AppendFormat($"<color=white>Position | </color>");
+		// _sb.AppendFormat(
+		// 		$"<color=green>( X:{spawn.Position.x:F2} Y:{spawn.Position.y:F2} Z:{spawn.Position.z:F2} )</color>\n");
+		
+		// Set marker position
+		var marker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		marker.transform.position = spawn.Position;
+		marker.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
+		marker.GetComponent<Renderer>().material.color = Color.white;
+		
+		// Push new marker data
+		var groupName = spawn.SpawnPoint.BotZoneName ?? "ungrouped";
+		_spawnZoneGroups[groupName].Add(new SpawnInstance()
 		{
-			// Create marker text
-			_sb.Clear();
-			_sb.AppendFormat($"Position | ");
-			_sb.AppendFormat($"( X:{spawn.Position.x:F2} Y:{spawn.Position.y:F2} Z:{spawn.Position.z:F2} )");
-			
-			// _sb.AppendFormat($"<color=white>Position | </color>");
-			// _sb.AppendFormat(
-			// 		$"<color=green>( X:{spawn.Position.x:F2} Y:{spawn.Position.y:F2} Z:{spawn.Position.z:F2} )</color>\n");
-			
-			// Set marker position
-			var marker = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-			marker.transform.position = spawn.Position;
-			marker.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
-			marker.GetComponent<Renderer>().material.color = Color.white;
-			
-			// Push new marker data
-			var groupName = spawn.SpawnPoint.BotZoneName ?? "ungrouped";
-			_spawnZoneGroups[groupName].Add(new SpawnInstance()
-			{
-					Position = spawn.Position,
-					Marker = marker,
-					GUIText = new GUIContent() { text = _sb.ToString() },
-					Display = new Rect()
-			});
-		}
+				Marker = marker, GUIText = new GUIContent() { text = _sb.ToString() }, Display = new Rect()
+		});
 	}
 	
 	private class SpawnInstance
 	{
-		public Vector3 Position;
 		public GameObject Marker;
 		public GUIContent GUIText;
 		public Rect Display;
